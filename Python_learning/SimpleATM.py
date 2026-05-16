229ac1ba72a90ea877aae4978f6ffc6d5279a537
Demmy <demolarise@gmail.com>
2026-05-16 03:07:39 -0400

    Attempts 4

--- Python_learning/SimpleATM.py
+++ Python_learning/SimpleATM.py
@@ -1,15 +1,15 @@
 correct_pin = "1234"
 attempts = 0
 
-while attempts < 3:
+while attempts < 4:
     user_input = input("please enter your PIN: ")
     if correct_pin == user_input:
         print("Access Granted! You're welcome!!")
         break
     else:
         attempts += 1
-        rem = 3 - attempts
+        rem = 4 - attempts
         if rem > 0:
          print(f"PIN incorrect, you have {rem} attempts left")
-if attempts == 3:
+if attempts == 4:
         print("Account Locked! Please contact your bank.")
